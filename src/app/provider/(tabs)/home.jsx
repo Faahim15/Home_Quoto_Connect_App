1aaeb46e0b9dc144e8b187931503c858eaed8d10
Faahim15 <abdulkaderfahim6@gmail.com>
2025-11-05 18:25:38 +0600

    profile get me api intregated

--- src/app/provider/(tabs)/home.jsx
+++ src/app/provider/(tabs)/home.jsx
@@ -1,4 +1,4 @@
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, ActivityIndicator } from "react-native";
 import HomeTopBar from "../../components/tabs/home/HomeTopBar";
 import { useState, useEffect } from "react";
 import FilterModal from "../../components/provider/home/FilteringModal";
@@ -7,19 +7,47 @@ import SearchBar from "../../components/tabs/home/SearchBar";
 import JobsHeader from "../../components/provider/home/JobsHeader";
 import { verticalScale } from "../../components/adaptive/Adaptiveness";
 import ServiceQuoteModal from "../../components/shared/modal/ServiceQuoteModal";
+import { useUserProfileQuery } from "../../../redux/features/apiSlices/user/userApiSlices";
 export default function ContractorHomeScreen() {
   const [showModal, setShowModal] = useState(false);
   const [showQuoteModal, setShowQuoteModal] = useState(false);
+
+  const {
+    data: profile,
+    isLoading: profileLoading,
+    error: profileError,
+  } = useUserProfileQuery();
+
+  console.log("profile data:", profile);
+
+  // ✅ Combined loading state
+  if (profileLoading) {
+    return (
+      <View className="flex-1 bg-[#F9FAFB] justify-center items-center">
+        <View className="flex-col items-center justify-center">
+          <ActivityIndicator
+            size="large"
+            color="#175994"
+            style={{ marginBottom: 16 }}
+          />
+          <Text className="font-poppins-500medium text-base text-[#565656]">
+            Loading...
+          </Text>
+        </View>
+      </View>
+    );
+  }
+  const userData = profile?.data?.user || null;
   function modalCloseHanlder() {
     setShowModal(false);
   }
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowQuoteModal(true);
-    }, 2000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowQuoteModal(true);
+  //   }, 2000);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
   return (
     <ScrollView
       contentContainerStyle={{
@@ -29,9 +57,9 @@ export default function ContractorHomeScreen() {
       showsVerticalScrollIndicator={false}
     >
       <View className="flex-1 bg-[#f9f9f9]">
-        {/* <View>
-          <HomeTopBar />
-        </View> */}
+        <View>
+          <HomeTopBar userData={userData} />
+        </View>
         <SearchBar onPress={() => setShowModal(true)} />
 
         <JobsHeader title="Todays Jobs" />
