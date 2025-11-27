c40ffafdc52b8bc79940b2ee19337e5b103f2ad2
Faahim15 <abdulkaderfahim6@gmail.com>
2025-11-27 18:25:26 +0600

    Provider filter added

--- src/app/components/tabs/home/services/provider/showAllServices.jsx
+++ src/app/components/tabs/home/services/provider/showAllServices.jsx
@@ -5,7 +5,6 @@ import {
   FlatList,
   TouchableOpacity,
   RefreshControl,
-  ActivityIndicator,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { scale, verticalScale } from "../../../../adaptive/Adaptiveness";
