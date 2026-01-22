036e3c7f72071cb84fce8013030c57b0a594492f
Faahim15 <abdulkaderfahim6@gmail.com>
2026-01-22 16:16:39 +0600

    raza home quoto connect latest one intgregated

--- src/app/provider/auth/backgroundCheck-Payment.jsx
+++ src/app/provider/auth/backgroundCheck-Payment.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View } from "react-native";
 import { router } from "expo-router";
 import { useStripe } from "@stripe/stripe-react-native";
 import { useState } from "react";
